9810432a14b70a62f34634078204288c3b1a3dcf
Titease <titien.carellas@epitech.eu>
2023-11-21 18:29:23 +0100

    [ADD] unzip folder and task1

---
+++ test.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+data = None
+
+def number_of_games(df):
+    return None
+
+number_of_games(data)
+
+def number_of_games_lfl(df):
+    return None
+
+number_of_games_lfl(data)
+
+
+
+#Calculate redsite winrate
+
+def redside_winrate(df):
+    return None
+
+redside_winrate(data)
